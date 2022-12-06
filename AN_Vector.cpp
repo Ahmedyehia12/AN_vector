e88d15701f65777384e4641f5118b3a9d781cafd
Nada Mohammed <naddaa26@gmail.com>
2022-12-06 23:12:51 +0200

    added comments

--- AN_Vector.cpp
+++ AN_Vector.cpp
@@ -3,11 +3,11 @@
 // Constructors and Big 4
 template<class T>
 AN_Vector<T> :: AN_Vector (int cap)
-    :size{0}, capacity{cap}, data{new T[capacity]}{
+    :size{0}, capacity{cap}, data{new T[capacity]}{  // constructor to initialize by specific capacity and size to zero
 }
 template<class T>
 AN_Vector<T> :: AN_Vector (T *ptr, int n)
-    :size{n}, capacity{n}, data{new T[capacity]}{
+    :size{n}, capacity{n}, data{new T[capacity]}{   // constructor to initialize by n elements from an array
     for (int i = 0;i<n;i++){
         data[i] = ptr[i];
     }
@@ -15,20 +15,26 @@ AN_Vector<T> :: AN_Vector (T *ptr, int n)
 template<class T>
 AN_Vector<T> :: AN_Vector (const AN_Vector &rhs)
     :size{rhs.size}, capacity{rhs.capacity}, data{new T[capacity]}{
+    // create a copy of the pointed data
     for (int i = 0;i<rhs.Size();i++){
         data[i] = rhs.data[i];
     }
 }
 template<class T>
 AN_Vector<T> :: ~AN_Vector(){
+    // delete allocated memory
     delete[] data;
 }
 template<class T>
 AN_Vector<T> &AN_Vector <T> :: operator=(const AN_Vector<T> &rhs){
+    // check if we are self assigning
     if(this != &rhs){
+        // delete current storage
         delete[] data;
+        // allocate new storage on the heap
         data = new T[rhs.capacity];
         size = rhs.size;
+        // perform the copy
         for(int i = 0;i<size;i++){
             data[i] = rhs.data[i];
         }
@@ -37,11 +43,15 @@ AN_Vector<T> &AN_Vector <T> :: operator=(const AN_Vector<T> &rhs){
 }
 template<class T>
 AN_Vector<T> &AN_Vector <T> :: operator=(AN_Vector &&rhs){
+    // check if we are self assigning
     if(this != &rhs){
+        // delete current storage
         delete[] data;
+        // copy the pointer to lhs from rhs
         data = rhs.data;
         size = rhs.size;
         capacity = rhs.capacity;
+        // null out the rhs pointer
         rhs.data = nullptr;
     }
     return *this;
@@ -50,10 +60,9 @@ AN_Vector<T> &AN_Vector <T> :: operator=(AN_Vector &&rhs){
 // Access operations
 template<class T>
 T& AN_Vector<T> :: operator[](int index){
+    // check if the index is in the range of the vector or not
     if(index < 0 || index >= size){
         throw out_of_range("Index element is out of range");
-        exit(-1);
-        //exit(-1);
     }
     return data[index];
 }
@@ -83,94 +92,6 @@ T AN_Vector<T>::pop_back() {
     }
     //decrementing the size of the vector
     return data[--size];
-}
-
-template<class T>
-void AN_Vector<T>::clear() {
-    //setting the size to 0
-    size = 0;
-}
-
-
-// Iterators
-template<class T>
-T* AN_Vector<T>::begin(){
-    return data;
-}
-template<class T>
-T* AN_Vector<T> :: end(){
-    return data + size;
-}
-
-// Comparison operations
-template<class T>
-bool AN_Vector<T>::operator==(const AN_Vector<T> & other) {
-    //checking if the sizes are equal
-    if(size != other.size)
-        return false;
-    //checking if the elements are equal
-    int cnt = 0;
-    for(int i=0;i<size;i++){
-        if(this->data[i] == other.data[i]){
-            cnt++;
-        }
-    }
-    //if the elements are equal then return true
-    if(cnt == size)
-        return true;
-    else
-        //if the elements are not equal then return false
-        return false;
-}
-template<class T>
-bool AN_Vector<T> :: operator< (const AN_Vector<T>& other)
-{
-    //checking if the sizes are equal
-     if (size != other.size) {
-        return false;}
-    //checking if the elements are equal
-     else {
-        for (int i = 0; i < size; i++) {
-            if (data[i] < other.data[i])
-                // if the elements are not equal then return false
-                return false;
-        }
-        //if the elements are equal then return true
-        return true;
-    }
-}
-
-// Capacity operations
-template<class T>
-int AN_Vector<T>::Size() const {
-    //returning the size of the vector
-    return size;
-}
-template<class T>
-int AN_Vector<T>::Capacity() const {
-    //returning the capacity of the vector
-    return capacity;
-}
-template<class T>
-int AN_Vector<T>::resize() {
-    //creating a new array with double the capacity
-        T *temp = new T[capacity*2];
-           capacity *= 2;
-        for (int i = 0; i < size; i++) {
-            temp[i] = data[i];
-        }
-        //deleting the old array
-        delete[] data;
-        //pointing the data to the new array
-        data = temp;
-        //delete[]temp;
-        //returning the new capacity
-    return capacity;
-}
-template<class T>
-bool AN_Vector<T>::empty() {
-    //checking if the size is 0
-    return size == 0;
 }
 template<class T>
 void AN_Vector<T>::erase(iterator i1, iterator i2) {
@@ -207,6 +128,11 @@ void AN_Vector<T>::erase(AN_Vector::iterator i) {
 
 }
 template<class T>
+void AN_Vector<T>::clear() {
+    //setting the size to 0
+    size = 0;
+}
+template<class T>
 void AN_Vector<T>::insert(AN_Vector::iterator i , T element) {
     //checking if the iterator is valid
     if(i>end() || i<begin()){
@@ -238,6 +164,89 @@ void AN_Vector<T>::insert(AN_Vector::iterator i , T element) {
 
         }
     }
+}
+
+// Iterators
+template<class T>
+T* AN_Vector<T>::begin(){
+    //  returns an iterator which points to the first element of the vector
+    return data;
+}
+template<class T>
+T* AN_Vector<T> :: end(){
+    //  returns an iterator which points to past the end element of the vector
+    return data + size;
+}
+
+// Comparison operations
+template<class T>
+bool AN_Vector<T>::operator==(const AN_Vector<T> & other) {
+    //checking if the sizes are equal
+    if(size != other.size)
+        return false;
+    //checking if the elements are equal
+    int cnt = 0;
+    for(int i=0;i<size;i++){
+        if(this->data[i] == other.data[i]){
+            cnt++;
+        }
+    }
+    //if the elements are equal then return true
+    if(cnt == size)
+        return true;
+    else
+        //if the elements are not equal then return false
+        return false;
+}
+template<class T>
+bool AN_Vector<T> :: operator< (const AN_Vector<T>& other)
+{
+    //checking if the sizes are equal
+    if (size != other.size) {
+        return false;}
+        //checking if the elements are equal
+    else {
+        for (int i = 0; i < size; i++) {
+            if (data[i] < other.data[i])
+                // if the elements are not equal then return false
+                return false;
+        }
+        //if the elements are equal then return true
+        return true;
+    }
+}
+
+// Capacity operations
+template<class T>
+int AN_Vector<T>::Size() const {
+    //returning the size of the vector
+    return size;
+}
+template<class T>
+int AN_Vector<T>::Capacity() const {
+    //returning the capacity of the vector
+    return capacity;
+}
+template<class T>
+int AN_Vector<T>::resize() {
+    //creating a new array with double the capacity
+    T *temp = new T[capacity*2];
+    capacity *= 2;
+    for (int i = 0; i < size; i++) {
+        temp[i] = data[i];
+    }
+    //deleting the old array
+    delete[] data;
+    //pointing the data to the new array
+    data = temp;
+    //delete[]temp;
+    //returning the new capacity
+    return capacity;
+}
+template<class T>
+bool AN_Vector<T>::empty() {
+    //checking if the size is 0
+    return size == 0;
 }
 
 // ostream operator overloading
--- main.cpp
+++ main.cpp
@@ -2,8 +2,5 @@
 
 int main() {
 
-
-AN_Vector<int> s (2);
-s.pop_back();
     return 0;
 }
